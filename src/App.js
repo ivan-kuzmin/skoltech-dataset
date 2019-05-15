6d3619f32857e33cd90df0fd49c147ac47491f40
Ivan Kuzmin <vania_kuzmin@mail.ru>
2019-05-15 21:46:10 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Hello = styled.div`
+  color: red;
+`;
+
+const App = () => {
+  return <Hello name=" ">Hello React!</Hello>;
+};
+
+export default App;
